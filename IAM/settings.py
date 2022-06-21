19ea7539c4dfdf2b97f726ecb31249fd01f4bcdd
Sujay Kumar/Cloud Platform /SRI-Bangalore/Associate/Samsung Electronics <sujay.kumar@partner.samsung.com>
2022-06-21 14:36:31 +0530

    added model test to reg_org

--- IAM/settings.py
+++ IAM/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3)du4!n(v=gfgempr(6$z&xb7z!#_r8qbweo*#z+ns0@7g^wa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
